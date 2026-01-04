3e542ffeed907b137cfa33619d5a51c891b7c84c
luwenjie <luwenjie@echo.tech>
2026-01-04 19:38:52 +0800

    e

--- packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/tbs/X5Api.kt
+++ packages/webview_flutter/webview_flutter_android/android/src/main/java/io/flutter/plugins/webviewflutter/tbs/X5Api.kt
@@ -31,42 +31,46 @@ class X5Api(private val context: Context) : AndroidX5WebViewApi {
     }
 
     override fun initX5Environment(callback: (Result<Boolean>) -> Unit) {
-        QbSdk.initX5Environment(context.applicationContext, object : PreInitCallback {
-            override fun onCoreInitFinished() {
-                // 内核初始化完成，可能为系统内核，也可能为系统内核
-                Log.d(TAG, "onCoreInitFinished")
-            }
+        if (QbSdk.canLoadX5(context)) {
+            QbSdk.initX5Environment(context.applicationContext, object : PreInitCallback {
+                override fun onCoreInitFinished() {
+                    // 内核初始化完成，可能为系统内核，也可能为系统内核
+                    Log.d(TAG, "onCoreInitFinished")
+                }
 
-            /**
-             * 预初始化结束
-             * 由于X5内核体积较大，需要依赖网络动态下发，所以当内核不存在的时候，默认会回调false，此时将会使用系统内核代替
-             * @param isX5 是否使用X5内核
-             */
-            override fun onViewInitFinished(isX5: Boolean) {
-                Log.d(TAG, "onViewInitFinished: isX5=$isX5")
-            }
-        })
-        val map = mutableMapOf<String, Any>()
-        map[TbsCoreSettings.TBS_SETTINGS_USE_SPEEDY_CLASSLOADER] = true
-        map[TbsCoreSettings.TBS_SETTINGS_USE_PRIVATE_CLASSLOADER] = true
-        map[TbsCoreSettings.TBS_SETTINGS_USE_DEXLOADER_SERVICE] = true
-        QbSdk.initTbsSettings(map)
-        QbSdk.setTbsListener(object : TbsListener {
-            override fun onDownloadFinish(p0: Int) {
+                /**
+                 * 预初始化结束
+                 * 由于X5内核体积较大，需要依赖网络动态下发，所以当内核不存在的时候，默认会回调false，此时将会使用系统内核代替
+                 * @param isX5 是否使用X5内核
+                 */
+                override fun onViewInitFinished(isX5: Boolean) {
+                    Log.d(TAG, "onViewInitFinished: isX5=$isX5")
+                }
+            })
+            val map = mutableMapOf<String, Any>()
+            map[TbsCoreSettings.TBS_SETTINGS_USE_SPEEDY_CLASSLOADER] = true
+            map[TbsCoreSettings.TBS_SETTINGS_USE_PRIVATE_CLASSLOADER] = true
+            map[TbsCoreSettings.TBS_SETTINGS_USE_DEXLOADER_SERVICE] = true
+            QbSdk.initTbsSettings(map)
+            QbSdk.setTbsListener(object : TbsListener {
+                override fun onDownloadFinish(p0: Int) {
 
-            }
+                }
 
-            override fun onInstallFinish(p0: Int) {
+                override fun onInstallFinish(p0: Int) {
 
-            }
+                }
 
-            override fun onDownloadProgress(p0: Int) {
+                override fun onDownloadProgress(p0: Int) {
 
-            }
+                }
 
-        })
-        callback(Result.success(QbSdk.canLoadX5(context)))
-        QbSdk.setDownloadWithoutWifi(true)
+            })
+            QbSdk.setDownloadWithoutWifi(true)
+            callback.invoke(true);
+        } else {
+            callback.invoke(false);
+        }
     }
 
     /**
@@ -76,11 +80,9 @@ class X5Api(private val context: Context) : AndroidX5WebViewApi {
      * @param version Version of the X5 kernel
      */
     override fun install(filePath: String, version: String) {
-        QbSdk.reset(context)
         Log.d(TAG, "install: filePath=$filePath, version=$version")
         if (filePath.isNotEmpty()) {
             QbSdk.installLocalTbsCore(context, version.toInt(), filePath)
-
         }
     }
 
